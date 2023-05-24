c93e64d3db3b8de55fffbe9f9620a8108fbb7c27
unknown <orevaorior@gmail.com>
2023-05-24 22:49:49 +0100

    animation completed

--- App.js
+++ App.js
@@ -1,52 +1,167 @@
 import { StatusBar } from 'expo-status-bar';
-import { Alert, Image, Text, TouchableOpacity, View } from 'react-native';
-import { launchImageLibrary } from 'react-native-image-picker';
-import { useState } from 'react';
-import { styles } from './styles';
+import { Alert, Image, Text, TouchableOpacity, View, Animated, ScrollView } from 'react-native';
+import { useEffect, useRef, useState } from 'react';
+import * as ImagePicker from 'expo-image-picker';
+import { LinearGradient } from 'expo-linear-gradient';
+import { loadFonts } from './expo-font';
+import getStyles  from './styles'; 
+import NavContext from './NavContext';
+
+
 
 export default function App() {
+  // FOR THE PROFILE IMAGE
   const [imageSource, setImageSource] = useState(require('./assets/man.png'));
+  // TO ENSURE FONTS ARE LOADED
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+  // TO CONTROL THE NAVIGATION
+  const [navAction, setNavAction] = useState(false);
+  const [left, setLeft] = useState(0);
+  
+  // FOR ANIMATIONS
+  const screenLeft = useState(new Animated.Value(0))[0]
+  const borderrad = useState(new Animated.Value(0))[0]
+  const screenScale = useState(new Animated.Value(1))[0]
+  // TO IMPLEMENT STYLING
+  const styles = getStyles(); 
 
-  const openImagePicker = () => {
-    const options = {
-      mediaType: 'photo',
-      includeBase64: false,
-    };
+  const animator =(animatorName, value, type=Animated.timing, time=250)=>{
+    return type(animatorName, {
+      toValue: value,
+      duration: time,
+      useNativeDriver: true,
+    }).start()
+  }
+  useEffect(()=>{
+    if(navAction){
+      animator(screenLeft, 250, Animated.spring);
+      animator(borderrad, 40, Animated.spring);
+      animator(screenScale, 0.85, Animated.spring);
+    }else{
+      animator(borderrad, 0);
+      animator(screenLeft, 0);
+      animator(screenScale, 1);
+    }
+  },[navAction])
 
-    launchImageLibrary(options, (response) => {
-      if (response.didCancel) {
-        console.log('User cancelled image picker');
-      } else if (response.error) {
-        console.log('Image picker error:', response.error);
-      } else if (response.assets && response.assets.length > 0) {
-        // Replace the image with the selected image
-        const selectedImage = response.assets[0];
-        setImageSource({ uri: selectedImage.uri });
+
+  // FUNCTION TO SELECT IMAGE AND SET IT TO THE IMAGESOURCE USESTATE
+  const selectImage = async () => {
+    try {
+      const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+      if (status !== 'granted') {
+        console.log('Permission to access media library denied');
+        return;
       }
-    });
+  
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        quality: 1,
+      });
+  
+      if (!result.canceled && result.assets.length > 0) {
+        const selectedAsset = result.assets[0];
+        setImageSource({ uri: selectedAsset.uri });
+      }
+    } catch (error) {
+      console.log('Error selecting image:', error);
+    }
   };
+  
+  
+  // LOAD FONTS
+  useEffect(() => {
+    try{
+      const loadAsync = async () => {
+        await loadFonts();
+        setFontsLoaded(true);
+      };
+  
+      loadAsync();
+    }catch(err){
+      console.log('an error occured')
+    }
+  }, []);
 
-  const closeNav =()=>{
-    Alert.alert('Image Clicked!');
+  // ENSURE FONT IS LOADED
+  if (!fontsLoaded) {
+    return <View />;
   }
 
   return (
-    <View style={styles.container}>
+    <NavContext.Provider value={{ navAction, setNavAction }}>
+        <View style={styles.container}>
       <View style={styles.subContainer}>
-        <TouchableOpacity onPress={closeNav} style={{ width: 35, marginLeft: 'auto' }}>
+        <TouchableOpacity onPress={()=>setNavAction(false)} style={{width: 35, marginLeft: 'auto'}}>
           <Image
             source={require('./assets/close.png')}
-            style={{ width: 35, height: 35 }}
+            style={{ width: 40, height: 40 }}
           />
         </TouchableOpacity>
-        <TouchableOpacity onPress={openImagePicker} style={{ width: '100%', display: 'flex', alignItems: 'center' }}>
+        <View style={{ width: '100%', display: 'flex', alignItems: 'flex-start', position: 'relative', bottom: 10 }}>
+          <TouchableOpacity onPress={selectImage} style={{ width: 100, height: 100, borderRadius: 100, overflow: 'hidden' }}>
+            <LinearGradient colors={['white', 'blue']} style={{width: 100, height: 100,  display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+              <Image
+                source={imageSource}
+                style={{ width: 93, height: 93, borderRadius: 50}}
+                />
+            </LinearGradient>
+          </TouchableOpacity>
+        </View>
+        <Text style={styles.firstname}>Oreva</Text> 
+        <Text style={styles.lastname}>Jovisam</Text>
+
+        <View style={styles.navList}>
+            <TouchableOpacity style={styles.button}>
+              <Image source={require('./assets/home.png')}
+                style={{ width: 25, height: 25 }}
+                />
+              <Text style={styles.navitem}>Home</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button}>
+              <Image source={require('./assets/pay.png')}
+                style={{ width: 25, height: 25 }}
+                />
+              <Text style={styles.navitem}>Make Payments</Text>
+            </TouchableOpacity>
+        </View>
+
+        
+
+      </View>
+      
+    {/* THE MAIN SCREEN */}
+    <Animated.View style={[styles.screenContainer, { transform: [{ translateX: screenLeft }, { scale: screenScale }], borderRadius: borderrad }]}>
+      {/* HEADER */}
+      <View style={styles.header}>
+        <TouchableOpacity onPress={() => setNavAction(true)}>
+            <Image
+              source={require('./assets/menu.png')}
+              style={{ width: 30, height: 30 }}
+              />
+        </TouchableOpacity>
+        <LinearGradient colors={['white', 'blue']} style={{width: 35, height: 35, borderRadius: 50, display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
           <Image
             source={imageSource}
-            style={{ width: 100, height: 100, position: 'relative', bottom: 20 }}
-          />
-        </TouchableOpacity>
+            style={{ width: 30, height: 30, borderRadius: 50}}
+            />
+        </LinearGradient>
       </View>
+
+      <View>
+        
+      </View>
+
+
+
+      
+    </Animated.View>
+
+
       <StatusBar style="auto" />
     </View>
+    </NavContext.Provider>
+
   );
 }
--- styles.js
+++ styles.js
@@ -1,6 +1,9 @@
 import { StyleSheet } from "react-native";
+import { useContext } from 'react';
+import NavContext from './NavContext';
 
-export const styles = StyleSheet.create({
+const getStyles = () => {
+    return StyleSheet.create({
   container: {
     width: '100%',
     height: '100%',
@@ -8,12 +11,80 @@ export const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'flex-start',
     backgroundColor: '#0E1E54',
-    padding: '5%',
   },
   subContainer:{
+    paddingLeft: '10%',
+    paddingRight: '3%',
     width: '70%',
     height: '80%',
-    backgroundColor: '#0000004F',
-    padding: '2%'
+    position: 'relative',
+    // backgroundColor: 'black',
+  },
+  firstname:{
+    color: 'white',
+    fontSize: 40,
+    textTransform: 'capitalize',
+    fontFamily: 'lili',
+    marginTop: 15,
+  },
+  lastname:{
+    color: 'white',
+    fontSize: 40,
+    textTransform: 'capitalize',
+    marginTop: -5,
+    fontFamily: 'lili',
+  },
+  navList: {
+    // backgroundColor: 'red',
+    width: '100%',
+    height: 250,
+    padding: 1,
+    marginTop: 25
+  },
+  button: {
+    display: 'flex',
+    flexDirection: 'row',
+    padding: 8,
+    backgroundColor: '#00105EFF',
+    marginBottom: 5,
+    borderRadius: 5,
+    elevation: 5, // For Android devices
+    shadowColor: '#000', // For iOS devices
+    shadowOpacity: 0.5,
+    shadowRadius: 5,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+  },
+  navitem: {
+    fontFamily: 'lili',
+    color: 'white',
+    marginLeft: 16,
+    fontSize: 15,
+    marginTop: 'auto',
+    marginBottom: 'auto',
+    
+  },
+  screenContainer: {
+    position: 'absolute',
+    width: '100%',
+    overflow: 'hidden',
+    paddingTop: 30,
+    height: '100%',
+    backgroundColor: '#D6FEFFFF',
+  },
+  header:{
+    width: '100%',
+    height: 30,
+    // backgroundColor: 'red',
+    paddingLeft: 20,
+    paddingRight: 20,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
   }
 });
+}
+
+export default getStyles;
